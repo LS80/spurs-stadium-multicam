e65013608901cbccddea84e9d69ea2785a3b5237
LS80 <github@lee-smith.me.uk>
2016-11-12 22:08:24 +0000

    Prototype for showing a busy indicator during stream loading

--- streamgrid.py
+++ streamgrid.py
@@ -3,31 +3,40 @@
 import sys
 
 from PyQt5.QtCore import Qt, QUrl, pyqtSignal
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QStackedLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QStackedLayout, QLabel
+from PyQt5.QtGui import QMovie, QPalette
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtMultimediaWidgets import QVideoWidget
 
 
 class StreamGrid(QWidget):
-    STREAMS = [
-        '04asu087t_1@409528',
-        '074tzyqju_1@394719',
-        '0i1p9ue46_1@409669',
-        '04et6qkjl_1@409668',
-        '0xorfu8d8_1@308855'
-    ]
+    streams_ready = pyqtSignal()
 
-    ready = pyqtSignal()
-
-    def __init__(self):
+    def __init__(self, streams):
+        self.streams = streams
         super().__init__()
+
         self.setAttribute(Qt.WA_QuitOnClose)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+
+        palette = QPalette()
+        palette.setColor(QPalette.Background, Qt.white)
+        self.setPalette(palette)
+
+        self.layout = QStackedLayout()
+        self.setLayout(self.layout)
+
+        label = QLabel()
+        label.setAlignment(Qt.AlignCenter)
+        loading = QMovie("loading.gif")
+        loading.start()
+        label.setMovie(loading)
+        self.layout.insertWidget(0, label)
 
         self.players = []
-        for stream in self.STREAMS:
+        for stream in self.streams:
             player = QMediaPlayer()
-            url = "http://streamamghdl-lh.akamaihd.net/i/{}/index_1_av-p.m3u8".format(stream)
-            player.setMedia(QMediaContent(QUrl(url)))
+            player.setMedia(QMediaContent(stream))
             player.mediaStatusChanged.connect(self.changedMediaStatus)
             player.play()
             self.players.append(player)
@@ -35,46 +44,38 @@ class StreamGrid(QWidget):
         grid_layout = QGridLayout()
         grid_layout.setSpacing(0)
         grid_layout.setContentsMargins(0, 0, 0, 0)
-        grid_widget = QWidget(self)
-        grid_widget.setLayout(grid_layout)
-
-        self.layout = QStackedLayout()
-        self.setLayout(self.layout)
+        self.grid_widget = QWidget()
+        self.grid_widget.setLayout(grid_layout)
 
         i = 0
         self.grid_widgets = []
         for row in range(2):
             for column in range(2):
-                video_widget = QVideoWidget(grid_widget)
+                video_widget = QVideoWidget()
                 self.players[i].setVideoOutput(video_widget)
                 self.grid_widgets.append(video_widget)
                 grid_layout.addWidget(video_widget, row, column)
                 i+=1
-        self.layout.addWidget(grid_widget)
+        self.layout.insertWidget(1, self.grid_widget)
 
-        video_widget = QVideoWidget(self)
+        video_widget = QVideoWidget()
         self.players[4].setVideoOutput(video_widget)
-        self.layout.addWidget(video_widget)
-
-        self.video_widget = QVideoWidget(self)
-        self.layout.addWidget(self.video_widget)
-
-        self.showStreamGrid()
+        self.layout.insertWidget(2, video_widget)
 
     def showStreamFullScreen(self, index):
         if index == 4:
-            self.layout.setCurrentIndex(1)
+            self.layout.setCurrentIndex(2)
         else:
             self.grid_widgets[index].show()
             for i, widget in enumerate(self.grid_widgets):
                 if i != index:
                     widget.hide()
-            self.layout.setCurrentIndex(0)
+            self.layout.setCurrentIndex(1)
 
     def showStreamGrid(self):
         for widget in self.grid_widgets:
             widget.show()
-        self.layout.setCurrentIndex(0)
+        self.layout.setCurrentIndex(1)
 
     def keyPressEvent(self, event):
         key = event.key()
@@ -88,11 +89,25 @@ class StreamGrid(QWidget):
     def changedMediaStatus(self, state):
         if all((player.mediaStatus() == QMediaPlayer.BufferedMedia) or
                (player.state() == QMediaPlayer.StoppedState) for player in self.players):
-            self.ready.emit()
+            self.streams_ready.emit()
 
+
+STREAM_IDS = [
+    '04asu087t_1@409528',
+    '074tzyqju_1@394719',
+    '0i1p9ue46_1@409669',
+    '04et6qkjl_1@409668',
+    '0xorfu8d8_1@308855'
+]
+
+STREAM_URL_FMT = "http://streamamghdl-lh.akamaihd.net/i/{}/index_1_av-p.m3u8"
+
+streams = [QUrl(STREAM_URL_FMT.format(id)) for id in STREAM_IDS]
 
 app = QApplication(sys.argv)
-main = StreamGrid()
-main.ready.connect(main.showFullScreen)
+
+viewer = StreamGrid(streams)
+viewer.showFullScreen()
+viewer.streams_ready.connect(viewer.showStreamGrid)
 
 app.exec_()
